603dc2741d8a027d40e33952ee6bccbcf5e0c65d
vhuhwavho <vhnetshjhb024@student.wethinkcode.co.za>
2025-05-10 19:45:27 +0200

    added the input from user to json file

--- JsonRequest.py
+++ JsonRequest.py
@@ -1,22 +1,28 @@
 import json
 
-def create_job_description_payload(user_input):
-    job_info= input("enter job title: ")
+def create_job_description_payload():
+    job_title= input("enter job title: ")
     job_info=input("please input a job description: ")
     experience = input("Please enter required experience: ")
+    context=input("please enter some context ")
+    
+    qualification = input("Please enter your qualification: ")
     
     
-    job_info= {
-        "job_description": user_input.strip()
+    info= {
+        "job title":job_title,
+        "job_description": job_info,
+        "experience":experience,
+        "context":context,
+        "qualification":qualification
         
         
     }
-    context=input("please enter some context")
-    background_info_about_me=input("Enter your cv infomartion this is to,make ressponses to be relative to you")
-    qualification = input("Please enter your qualification: ")
-
-    # Convert dictionary to JSON string (pretty printed)
     
-    print("...............................................")
-    json_payload = json.dumps(payload, indent=2)
+
+
+    
+    
+    json_payload = json.dumps(info, indent=2)
     return json_payload
+print(create_job_description_payload())
