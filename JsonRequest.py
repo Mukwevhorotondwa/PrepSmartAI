c693db6c5cefbfb780a17f8a2ee644e0e1a579f8
Tsatsawane <138806352+Charity2001@users.noreply.github.com>
2025-05-10 18:09:33 +0200

    Update JsonRequest.py
    i was adding experience and qualification variables and asking the user title of job.

--- JsonRequest.py
+++ JsonRequest.py
@@ -1,7 +1,9 @@
 import json
 
 def create_job_description_payload(user_input):
-    job_info=input("please input a job description")
+    job_info= input("enter job title: ")
+    job_info=input("please input a job description: ")
+    experience = input("Please enter required experience: ")
     
     
     job_info= {
@@ -11,6 +13,7 @@ def create_job_description_payload(user_input):
     }
     context=input("please enter some context")
     background_info_about_me=input("Enter your cv infomartion this is to,make ressponses to be relative to you")
+    qualification = input("Please enter your qualification: ")
 
     # Convert dictionary to JSON string (pretty printed)
     
